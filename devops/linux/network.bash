9b150136d65178bdda068a26c637310bcbc86fce
Edinaldo Rodrigues <edinaldorodriguesceara@gmail.com>
2024-12-12 20:16:57 -0300

    mariadb

--- devops/linux/network.bash
+++ devops/linux/network.bash
@@ -5,4 +5,4 @@ set -e
 cat /etc/hosts
 
 echo > /dev/tcp/localhost/9000 && echo "PHP-FPM is running."
-echo > /dev/tcp/mysql/3306 && echo "Mysql is running."
+echo > /dev/tcp/mariadb/3306 && echo "Mariadb is running."
