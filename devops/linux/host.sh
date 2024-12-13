cb5a35a4753f473c0be50e9b9f46d11182fd4da6
Edinaldo Rodrigues <edinaldorodriguesceara@gmail.com>
2024-12-13 00:39:39 -0300

    ec2

--- devops/linux/host.sh
+++ devops/linux/host.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+host php-edinaldo-rodrigues.localhost
+host blog-edinaldo-rodrigues.localhost
+host docker-edinaldo-rodrigues.localhost
+
 host php-edinaldo-rodrigues.dreamsquad.com.br
 host blog-edinaldo-rodrigues.dreamsquad.com.br
 host docker-edinaldo-rodrigues.dreamsquad.com.br
