114ba999bc62fc2d7978d3554b782e452f6f06da
Edinaldo Rodrigues <edinaldorodriguesceara@gmail.com>
2024-12-12 23:15:21 -0300

    ec2

--- devops/compose/up.sh
+++ devops/compose/up.sh
@@ -4,6 +4,8 @@ set -e
 
 . devops/compose/.env
 
+git update-index --assume-unchanged devops/ec2/.env
+
 ARG_USER_UID=$ARG_USER_UID \
 ARG_USER_GID=$ARG_USER_GID \
 DOCKER_DEFAULT_PLATFORM=$DOCKER_DEFAULT_PLATFORM \
